1cf4b0961eb9063d1f8a533cf702d06266b589ac
DK <kd.000.000.1@gmail.com>
2021-12-24 23:56:52 -0500

    refactored

--- src/app/features/appointment-report/components/appointment-summary/appointment-summary.component.ts
+++ src/app/features/appointment-report/components/appointment-summary/appointment-summary.component.ts
@@ -1,7 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { AppointmentReportModel } from '../../models/appointment-report.model';
-import {BehaviorSubject, EMPTY, from, Observable} from 'rxjs';
-import {count, tap} from 'rxjs/operators';
+import { BehaviorSubject, EMPTY, from, Observable } from 'rxjs';
+import { count, tap } from 'rxjs/operators';
+import { AppointmentSummaryModel} from '../../models/appointment-summary.model';
 
 @Component({
   selector: 'app-appointment-summary',
@@ -10,49 +11,39 @@ import {count, tap} from 'rxjs/operators';
 })
 export class AppointmentSummaryComponent implements OnInit {
 
+  @Input() input: AppointmentReportModel[] | undefined | null;
+  model = new AppointmentSummaryModel();
+
   constructor() { }
 
-  @Input() data: AppointmentReportModel[] | undefined | null;
+  async ngOnInit(): Promise<void> {
+    this.model = await this.calculateMetricsAsync();
+  }
 
-  // async calculations example:
-  Verified$?: Observable<number> = new BehaviorSubject(0);
+  private async calculateMetricsAsync(): Promise<AppointmentSummaryModel> {
 
-  // sync calculations example:
-  VerifiedPescent = 0;
-  MetricsAmount = 0;
-  Scheduled = 0;
-  Rescheduled = 0;
-  NotVerified = 0;
-  Cancelled = 0;
-  NotQualified = 0;
-  RepCancelled = 0;
-  DoctorCancelled = 0;
-  Confirmed = 0;
-  Completed = 0;
-  Interested = 0;
-  NotInterested = 0;
-  Producing = 0;
+    const model = new AppointmentSummaryModel();
 
-  ngOnInit(): void {
+    const promise = new Promise<AppointmentSummaryModel>(resolve => {
+      model.Verified = this.input?.filter(item => item.Status === 'Verified').length ?? 0;
+      model.Scheduled = this.input?.filter(item => item.Status === 'Scheduled').length ?? 0;
+      model.Rescheduled = this.input?.filter(item => item.Status === 'Rescheduled').length ?? 0;
+      model.NotVerified = this.input?.filter(item => item.Status === 'Not Verified').length ?? 0;
+      model.Cancelled = this.input?.filter(item => item.Status === 'Cancelled').length ?? 0;
+      model.NotQualified = this.input?.filter(item => item.Status === 'Not Qualified').length ?? 0;
+      model.RepCancelled = this.input?.filter(item => item.Status === 'Rep Cancelled').length ?? 0;
+      model.DoctorCancelled = this.input?.filter(item => item.Status === 'Doctor Cancelled').length ?? 0;
+      model.Confirmed = this.input?.filter(item => item.Status === 'Confirmed').length ?? 0;
+      model.Completed = this.input?.filter(item => item.Status === 'Completed').length ?? 0;
+      model.Interested = this.input?.filter(item => item.Status === 'Interested').length ?? 0;
+      model.NotInterested = this.input?.filter(item => item.Status === 'Not Interested').length ?? 0;
+      model.Producing = this.input?.filter(item => item.Status === 'Producing').length ?? 0;
+      let metricsAmount = 0;
+      Object.values(model).forEach(metric => metricsAmount += metric);
+      model.MetricsAmount = metricsAmount;
+      resolve(model);
+      });
 
-    // async calculations example - can be used in template as ({{ (Verified$ | async) }}) + ({{ (Scheduled$ | async) }})
-    this.Verified$ = from(this.data ?? EMPTY)
-      .pipe(
-        count(value => value.Status === 'Verified'),
-      tap((verified) => {
-          this.NotQualified = this.data?.filter(item => item.Status === 'Not Qualified').length ?? 0;
-          this.RepCancelled = this.data?.filter(item => item.Status === 'Rep Cancelled').length ?? 0;
-
-          this.MetricsAmount = verified + this.Scheduled + this.Rescheduled + this.RepCancelled;
-          this.VerifiedPescent = verified / this.MetricsAmount;
-        })
-      );
-
-    // sync calculations example:
-    // this.NotQualified = this.data?.filter(item => item.Status === 'Not Qualified').length ?? 0;
-    // this.RepCancelled = this.data?.filter(item => item.Status === 'Rep Cancelled').length ?? 0;
-
-    // this.MetricsAmount = this.Scheduled + this.Rescheduled + this.RepCancelled;
-    // ToDo: ...
+    return promise;
   }
 }
---
+++ src/app/features/appointment-report/models/appointment-summary.model.ts
@@ -0,0 +1,17 @@
+export class AppointmentSummaryModel {
+  Verified = 0;
+  Scheduled = 0;
+  Rescheduled = 0;
+  NotVerified = 0;
+  Cancelled = 0;
+  NotQualified = 0;
+  RepCancelled = 0;
+  DoctorCancelled = 0;
+  Confirmed = 0;
+  Completed = 0;
+  Interested = 0;
+  NotInterested = 0;
+  Producing = 0;
+
+  MetricsAmount = 0;
+}
